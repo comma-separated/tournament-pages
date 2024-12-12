bda3145227b986d604db1a68068471ef2733a4ce
gothchibjo <max@mxmv.ru>
2024-12-12 22:25:19 +0300

    Updates

---
+++ assets/index-BLZVGS78.js
@@ -0,0 +1,1 @@
+import{l as e,K as s}from"./index-Ckrw3-q5.js";const t=e({__name:"index",setup(n){return s().push("/"),()=>{}}});export{t as default};
--- assets/index-CncdIwtx.js
+++
@@ -1,1 +0,0 @@
-import{l as e,K as s}from"./index-Cgt8FsMp.js";const t=e({__name:"index",setup(n){return s().push("/"),()=>{}}});export{t as default};
