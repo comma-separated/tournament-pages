fd96d295c03f1e335a19635dec423bb0b712dee4
gothchibjo <max@mxmv.ru>
2024-12-24 00:25:07 +0300

    Updates

--- assets/index-BLZVGS78.js
+++
@@ -1,1 +0,0 @@
-import{l as e,K as s}from"./index-Ckrw3-q5.js";const t=e({__name:"index",setup(n){return s().push("/"),()=>{}}});export{t as default};
---
+++ assets/index-DiEQkUJD.js
@@ -0,0 +1,1 @@
+import{l as e,K as s}from"./index-Beozyjld.js";const t=e({__name:"index",setup(n){return s().push("/"),()=>{}}});export{t as default};
